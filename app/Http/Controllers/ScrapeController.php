96a81c8ad4023630eb7ebadfea6e116ae1927a2b
repiyann <muhrepiyan26@gmail.com>
2024-01-27 01:34:47 +0700

    test: test for search by category

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -59,23 +59,37 @@ class ScrapeController extends Controller
             $crawler = new Crawler($html);
 
             // Extract specific information from the website
-            $recipes = $crawler->filter('._recipe-card')->each(function (Crawler $node, $i) {
-                $title = trim($node->filter('h3.card-title a')->text());
-                $thumbnail = $node->filter('.thumbnail img')->attr('data-src'); // still null not receiving any data
-                $key = $node->filter('div[data-id]')->attr('data-id'); // possibly in use
-                $cookTime = trim($node->filter('a[href*="time="] span')->text());
-                $difficulty = trim($node->filter('a[href*="difficulty="]')->text());
+            $categories = $crawler->filter('._category-select')->each(function (Crawler $node, $i) {
+                $title = trim($node->filter('a.dropdown-item data-tracking')->text());
+                $resUrl = $node->filter('a[href]')->attr('href');
+                $split = explode(' ', $title);
+
+                // Check if 'Menu' is present and remove it
+                if (in_array('Menu', $split)) {
+                    $indexToRemove = array_search('Menu', $split);
+                    unset($split[$indexToRemove]);
+                }
+
+                // Join the array elements with '-'
+                // $results = implode('-', $split);
+
+                // Assuming $href is your 'a' tag's href attribute
+                $href = 'your/a/href/here';
+
+                // Split the href by '/'
+                $key = explode('/', $href);
+
+                // Get the second-to-last element
+                $key = $key[count($key) - 2];
 
                 return [
                     'title' => $title,
-                    'thumb' => $thumbnail,
+                    'resUrl' => $resUrl,
                     'key' => $key,
-                    'cookTime' => $cookTime,
-                    'difficulty' => $difficulty,
                 ];
             });
 
-            return view('scraped-data', ['recipes' => $recipes, 'searchQuery' => $searchQuery]);
+            return view('scraped-data', ['categories' => $categories, 'searchQuery' => $searchQuery]);
         } catch (\Exception $e) {
             // Log or display the error
             dd($e->getMessage());
